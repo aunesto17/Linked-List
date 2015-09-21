085fa9d99891440bccd8c544b1c52484976b1d1a
Alex Baylon <aunesto15@gmail.com>
2015-09-21 09:14:45 -0500

    Initial Commit

---
+++ AED_Theory/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AED_Theory
+//
+//  Created by Alexander Arturo Baylon Ibanez on 21/09/15.
+//  Copyright © 2015 Alexander Arturo Baylon Ibanez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
